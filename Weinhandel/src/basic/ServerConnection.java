a4a5c124c02d5c3ef762e2d54044ba0a8909cd43
SebastianWoetzel <Sebastian.woetzel@gmail.com>
2014-01-15 18:34:49 +0100

    Change-Id: Ie5c73d356932367dce921287322be966e6a17d08

--- Weinhandel/src/basic/ServerConnection.java
+++ Weinhandel/src/basic/ServerConnection.java
@@ -1,6 +1,35 @@
 package basic;
 
-public class ServerConnection {
-	
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ServerConnection {
+ 
+private static Connection connect = null;
+ 
+ 
+ public ServerConnection(){
+  establishDBConnection();
+//  Test myTest = new Test(connect);
+ }
+
+ public void establishDBConnection(){
+  System.out.println("JDBC-Connection Test");
+  try {
+   Class.forName("com.mysql.jdbc.Driver");
+   connect = DriverManager.getConnection("jdbc:mysql://localhost/weinhandlung", "root", "");
+   System.out.println("...verbunden");
+  } catch (ClassNotFoundException e) {
+   System.out.println("Fehler: " + e.getMessage());
+  } catch (SQLException e) {
+   System.out.println("Fehler: " + e.getMessage());
+  }
+  
+ }
+
+
+ public static void main(String[]args) throws SQLException{
+  new ServerConnection();  
+ }
 }
