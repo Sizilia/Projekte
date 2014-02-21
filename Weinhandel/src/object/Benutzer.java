62cb78383e7184398bf02d6b79df98f7e605357b
SebastianWoetzel <sebastian.woetzel@gmail.com>
2014-02-21 15:26:35 +0100

    Update Benutzer.java

--- Weinhandel/src/object/Benutzer.java
+++ Weinhandel/src/object/Benutzer.java
@@ -5,7 +5,6 @@ public class Benutzer {
 	private int    nID;
 	private String cUsername;
 	private String cPassw;
-	private java.util.Date dDatum_ein;
 	
 	public int getID(){
 		return nID;
@@ -25,11 +24,5 @@ public class Benutzer {
 	}
 	public void setPassw(String cPassw) {
 		this.cPassw = cPassw;
-	}
-	public java.util.Date getDatum_ein() {
-		return dDatum_ein;
-	}
-	public void setDatum_ein(java.util.Date dDatum_ein) {
-		this.dDatum_ein = dDatum_ein;
 	}
 }
